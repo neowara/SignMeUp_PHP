313eb05e19cdee81f28ad54e37c85758568aa3df
Christian <32764514+neowara@users.noreply.github.com>
2019-04-30 15:52:49 +0200

    Finished the CSS for the page

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php require 'global.php' ?>
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab 1</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet"> 
+</head>
+
 <body>
     <section class="content">
     <h2>Log in</h2>
